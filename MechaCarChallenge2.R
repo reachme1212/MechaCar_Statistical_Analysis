df44e529d7caa69a0fd8cea896c6ce86f9c83f60
Sangee_uofa <reachme1212@gmail.com>
2021-12-24 16:35:52 -0700

    saved R file

--- MechaCarChallenge2.R
+++ MechaCarChallenge2.R
@@ -24,4 +24,14 @@ lot_summary <- mecha_coil  %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PS
 plt <- ggplot(mecha_coil,aes(x=Manufacturing_Lot,y=PSI))
 plt + geom_boxplot() 
 
+# ttest 
+t.test(mecha_coil$PSI,mu=1500)
 
+# ttest for all lots 
+lot1 <- subset(mecha_coil, Manufacturing_Lot=="Lot1")
+lot2 <- subset(mecha_coil, Manufacturing_Lot=="Lot2")
+lot3 <- subset(mecha_coil, Manufacturing_Lot=="Lot3")
+
+t.test(lot1$PSI,mu=1500)
+t.test(lot2$PSI,mu=1500)
+t.test(lot3$PSI,mu=1500)
